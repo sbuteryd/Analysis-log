993c763b3b0cccfc390e196c90c3e5d345773733
Russell <lihangen@lihangendeMBP.lan>
2018-11-23 14:45:48 +0800

    feat: remove dateabase directory

--- log_analysis.py
+++ log_analysis.py
@@ -3,8 +3,16 @@ import psycopg2
 conn = psycopg2.connect("dbname=news")
 
 cursor = conn.cursor()
-cursor.execute("select * from articles ")
+# cursor.execute("select title from articles ")
+cursor.execute("select path from log limit 10")
+
 
 results = cursor.fetchall()
-print(results)
+for i in results:
+    remove_dir_root = i[0]
+    if len(remove_dir_root) >1:
+        remove_dir_art = remove_dir_root[9:]
+        remove_dash = remove_dir_art.replace('-',' ')
+        print(remove_dash)
+
 conn.close()
