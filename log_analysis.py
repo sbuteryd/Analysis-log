e0b02a4c218c3cb50b8ecf92b3bc487550088da6
Russell <lihangen@74.142.41.120.broad.xm.fj.dynamic.163data.com.cn>
2018-11-24 14:01:20 +0800

    get polular artilce authors

--- log_analysis.py
+++ log_analysis.py
@@ -11,11 +11,23 @@ def get_most_pupular():
     cursor.execute(
         "select substring,count(substring) as num from log_remove_root_path_fist  join articles  on log_remove_root_path_fist.substring = articles.slug group by log_remove_root_path_fist.substring order by num desc limit 3 ;")
     results = cursor.fetchall()
+    print("most popular three articles of all time?")
     for i in results:
         em_dash = i[0].replace('-', ' ')
-        print(em_dash + " -- " + str(i[1]))
+        print(em_dash + " -- " + str(i[1])+' views')
+
+
+
+
+def get_pupular_authors():
+    cursor.execute("create view articles_authors_second as select slug,name from articles join authors on articles.author = authors.id;")
+    cursor.execute("select name ,count(name) as num  from articles_authors join log_remove_root_path_fist on articles_authors.slug =log_remove_root_path_fist.substring group by name")
+    results = cursor.fetchall()
+    print("\nmost popular article authors of all time?")
+    for i in results:
+        em_dash = i[0].replace('-', ' ')
+        print(em_dash + " -- " + str(i[1])+' views')
     conn.close()
 
 get_most_pupular()
-
-
+get_pupular_authors()
