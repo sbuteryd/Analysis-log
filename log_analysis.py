f60ca4aabb95aa71ca5da198d81de59fd26094a8
Russell <lihangen@9.143.41.120.broad.xm.fj.dynamic.163data.com.cn>
2018-11-24 11:02:39 +0800

    refactor: modular,use function

--- log_analysis.py
+++ log_analysis.py
@@ -1,16 +1,21 @@
 import psycopg2
 
-conn = psycopg2.connect("dbname=news")
 
-cursor = conn.cursor()
 # cursor.execute("select title from articles ")
-cursor.execute("create view log_remove_root_path_test as select substring(path,10) from log;")
-cursor.execute("select substring,count(substring) as num from log_remove_root_path join articles  on log_remove_root_path.substring = articles.slug group by log_remove_root_path.substring order by num desc limit 3 ;")
+conn = psycopg2.connect("dbname=news")
+cursor = conn.cursor()
 
-results = cursor.fetchall()
 
-for i in results:
-    em_dash = i[0].replace('-',' ')
-    print(em_dash+" -- "+str(i[1]))
+def get_most_pupular():
+    cursor.execute("create view log_remove_root_path_test as select substring(path,10) from log;")
+    cursor.execute(
+        "select substring,count(substring) as num from log_remove_root_path join articles  on log_remove_root_path.substring = articles.slug group by log_remove_root_path.substring order by num desc limit 3 ;")
+    results = cursor.fetchall()
+    for i in results:
+        em_dash = i[0].replace('-', ' ')
+        print(em_dash + " -- " + str(i[1]))
+    conn.close()
 
-conn.close()
+get_most_pupular()
+
+
