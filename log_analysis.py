e9bc67b2df4aad237bdf825e20df9e5d853ecb0e
Russell <lihangen@lihangendeMacBook-Pro.local>
2018-11-24 10:34:54 +0800

    print most popular result

--- log_analysis.py
+++ log_analysis.py
@@ -8,6 +8,7 @@ cursor.execute("create view log_remove_root_path_test as select substring(path,1
 cursor.execute("select substring,count(substring) as num from log_remove_root_path join articles  on log_remove_root_path.substring = articles.slug group by log_remove_root_path.substring order by num desc limit 3 ;")
 
 results = cursor.fetchall()
-print(results)
+for i in results:
+    print(i[0]+" "+str(i[1]))
 
 conn.close()
