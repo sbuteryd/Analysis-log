236f16bc87e645c7c0e4f731228c1a7274dd3c38
Russell <lihangen@lihangendeMacBook-Pro.local>
2018-11-24 10:23:33 +0800

    feat:get most popular three aricles

--- log_analysis.py
+++ log_analysis.py
@@ -4,15 +4,10 @@ conn = psycopg2.connect("dbname=news")
 
 cursor = conn.cursor()
 # cursor.execute("select title from articles ")
-cursor.execute("select path from log limit 10")
-
+cursor.execute("create view log_remove_root_path_test as select substring(path,10) from log;")
+cursor.execute("select substring,count(substring) as num from log_remove_root_path join articles  on log_remove_root_path.substring = articles.slug group by log_remove_root_path.substring order by num desc limit 3 ;")
 
 results = cursor.fetchall()
-for i in results:
-    remove_dir_root = i[0]
-    if len(remove_dir_root) >1:
-        remove_dir_art = remove_dir_root[9:]
-        remove_dash = remove_dir_art.replace('-',' ')
-        print(remove_dash)
+print(results)
 
 conn.close()
