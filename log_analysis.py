0af3839886fbec2cd791db33f9f43d3dd6ed0312
sbuteryd@qq.com <sbuteryd@qq.com>
2018-11-30 11:33:53 +0800

    feat:fix bug

--- log_analysis.py
+++ log_analysis.py
@@ -5,44 +5,68 @@ import psycopg2
 conn = psycopg2.connect("dbname=news")
 cursor = conn.cursor()
 
-
 def get_most_pupular():
-    cursor.execute("create view log_remove_root_path_fist as select substring(path,10),status from log where status = '200 OK';")
+    cursor.execute("create view log_fist "
+                   "as select substring(path,10),status from "
+                   "log where status = '200 OK';")
     cursor.execute(
-        "select substring,count(substring) as num from log_remove_root_path_fist  join articles  on log_remove_root_path_fist.substring = articles.slug group by log_remove_root_path_fist.substring order by num desc limit 3 ;")
+        "select substring,count(substring) "
+        "as num from log_fist  "
+        "join articles  on log_fist.substring = articles.slug "
+        "group by log_fist.substring order by num desc limit 3 ;")
     results = cursor.fetchall()
     print("\nmost popular three articles of all time?")
     for i in results:
         em_dash = i[0].replace('-', ' ')
         print(em_dash + " -- " + str(i[1])+' views')
 
-
-
-
 def get_pupular_authors():
-    cursor.execute("create view articles_authors_second as select slug,name from articles join authors on articles.author = authors.id;")
-    cursor.execute("select name ,count(name) as num  from articles_authors_second join log_remove_root_path_fist on articles_authors_second.slug =log_remove_root_path_fist.substring group by name order by num desc")
+    cursor.execute("create view authors_second as "
+                   "select slug,name from articles join "
+                   "authors on articles.author = authors.id;")
+    cursor.execute("select name ,count(name) as num  "
+                   "from authors_second join log_fist "
+                   "on authors_second.slug = "
+                   "log_fist.substring group by name order by num desc")
     results = cursor.fetchall()
     print("\nmost popular article authors of all time?")
     for i in results:
         em_dash = i[0].replace('-', ' ')
         print(em_dash + " -- " + str(i[1])+' views')
 
-    conn.close()
+def calculate_percentage():
+   cursor.execute("create view calculate_third as select count(*) from log;")
 
-
-def calculate_percentage(num1,num2):
-   # cursor.execute("create view calculate_connect_third as select count(*) from log;")
-   # cursor.execute("create view only_date_third as select date(time),status from log where status = '404 NOT FOUND' ")
    # cursor.execute("select date ,count(*) as num from only_date_times group by only_date_times.date order by num desc limit 1;")
-       num1 = float(num1)
-       num2 = float(num2)
-       percentage = '{0:.2f}'.format((num1 / num2 * 100))
-       return percentage
+   #     num1 = float(num1)
+   #     num2 = float(num2)
+   #     percentage = '{0:.2f}'.format((num1 / num2 * 100))
+   #     return percentage
+   cursor.execute("select * from calculate_third")
+   results = cursor.fetchall()
 
+   sum = results[0]
+   return  sum
+
+
+def connect_error(sum_all):
+    cursor.execute("create view error_third as select date(time),status "
+                   "from log where status = '404 NOT FOUND' ")
+    cursor.execute("select date ,count(*) as num from "
+                   "error_third group by error_third.date"
+                   " order by num desc limit 1;")
+
+    results = cursor.fetchall()
+
+
+    error_sum = results[0]
+    date = results[0][0]
+    error_max = error_sum[1]
+    print("\nOn which days did more than 1% of requests lead to errors?")
+    text_sum = "{},{} errors".format(date,(error_max/sum_all) *100)
+    print(text_sum)
 
 get_most_pupular()
 get_pupular_authors()
-save = calculate_percentage(1234,1677735)
-print("\nOn which days did more than 1% of requests lead to errors?")
-print("July 17, 2016 "+save+"% error")
+sum_all = calculate_percentage()
+connect_error(sum_all[0])
