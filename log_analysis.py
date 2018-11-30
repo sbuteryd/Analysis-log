734b7373953b31801297fd92eaac4cb5fcb0c299
sbuteryd@qq.com <sbuteryd@qq.com>
2018-11-30 11:50:10 +0800

    fix:fix format use pycodestyle

--- log_analysis.py
+++ log_analysis.py
@@ -5,6 +5,7 @@ import psycopg2
 conn = psycopg2.connect("dbname=news")
 cursor = conn.cursor()
 
+
 def get_most_pupular():
     cursor.execute("create view log_fist "
                    "as select substring(path,10),status from "
@@ -15,10 +16,12 @@ def get_most_pupular():
         "join articles  on log_fist.substring = articles.slug "
         "group by log_fist.substring order by num desc limit 3 ;")
     results = cursor.fetchall()
+
     print("\nmost popular three articles of all time?")
     for i in results:
         em_dash = i[0].replace('-', ' ')
         print(em_dash + " -- " + str(i[1])+' views')
+
 
 def get_pupular_authors():
     cursor.execute("create view authors_second as "
@@ -34,19 +37,13 @@ def get_pupular_authors():
         em_dash = i[0].replace('-', ' ')
         print(em_dash + " -- " + str(i[1])+' views')
 
+
 def calculate_percentage():
-   cursor.execute("create view calculate_third as select count(*) from log;")
-
-   # cursor.execute("select date ,count(*) as num from only_date_times group by only_date_times.date order by num desc limit 1;")
-   #     num1 = float(num1)
-   #     num2 = float(num2)
-   #     percentage = '{0:.2f}'.format((num1 / num2 * 100))
-   #     return percentage
-   cursor.execute("select * from calculate_third")
-   results = cursor.fetchall()
-
-   sum = results[0]
-   return  sum
+    cursor.execute("create view calculate_third as select count(*) from log;")
+    cursor.execute("select * from calculate_third")
+    results = cursor.fetchall()
+    sum = results[0]
+    return sum
 
 
 def connect_error(sum_all):
@@ -55,18 +52,18 @@ def connect_error(sum_all):
     cursor.execute("select date ,count(*) as num from "
                    "error_third group by error_third.date"
                    " order by num desc limit 1;")
-
     results = cursor.fetchall()
-
-
     error_sum = results[0]
     date = results[0][0]
     error_max = error_sum[1]
     print("\nOn which days did more than 1% of requests lead to errors?")
-    text_sum = "{},{} errors".format(date,(error_max/sum_all) *100)
+    text_sum = "{},{} errors".format(date, (error_max/sum_all) * 100)
     print(text_sum)
 
+
 get_most_pupular()
+
 get_pupular_authors()
+
 sum_all = calculate_percentage()
 connect_error(sum_all[0])
