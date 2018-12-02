53a281629ad7891b31d27efaac05f34a00fd1c10
sbuteryd@qq.com <sbuteryd@qq.com>
2018-12-02 09:40:16 +0800

    fix:psqcode styles

--- log_analysis.py
+++ log_analysis.py
@@ -6,14 +6,18 @@ cursor = conn.cursor()
 
 
 def get_most_pupular():
-    cursor.execute("create view log_fist "
-                   "as select substring(path,10),status from "
-                   "log where status = '200 OK';")
+    cursor.execute("""
+    create view log_fist
+    as select substring(path,10),status from
+    log where status = '200 OK';
+    """)
     cursor.execute(
-        "select substring,count(substring) "
-        "as num from log_fist  "
-        "join articles  on log_fist.substring = articles.slug "
-        "group by log_fist.substring order by num desc limit 3 ;")
+        """
+        select substring,count(substring)
+        as num from log_fist
+        join articles  on log_fist.substring = articles.slug
+        group by log_fist.substring order by num desc limit 3 ;
+        """)
     results = cursor.fetchall()
 
     print("\nmost popular three articles of all time?")
@@ -23,14 +27,18 @@ def get_most_pupular():
 
 
 def get_pupular_authors():
-    cursor.execute("create view authors_second as "
-                   "select slug,name from articles join "
-                   "authors on articles.author = authors.id;")
+    cursor.execute("""
+    create view authors_second as
+    select slug,name from articles join
+    authors on articles.author = authors.id;
+    """)
 
-    cursor.execute("select name ,count(name) as num  "
-                   "from authors_second join log_fist "
-                   "on authors_second.slug = "
-                   "log_fist.substring group by name order by num desc")
+    cursor.execute("""
+    select name ,count(name) as num
+    from authors_second join log_fist
+    on authors_second.slug =
+    log_fist.substring group by name order by num desc
+    """)
     results = cursor.fetchall()
     print("\nmost popular article authors of all time?")
     for i in results:
@@ -48,12 +56,15 @@ def calculate_percentage():
 
 def connect_error():
 
-    cursor.execute("create view error_third as select date(time),status "
-                   "from log where status = '404 NOT FOUND' ")
+    cursor.execute("""
+    create view error_third as select date(time),status
+    from log where status = '404 NOT FOUND'
+    """)
 
-    cursor.execute("select date ,count(*) as num from "
-                   "error_third group by error_third.date"
-                   " order by num desc limit 1;")
+    cursor.execute("""
+    select date ,count(*) as num from
+    error_third group by error_third.date
+    order by num desc limit 1;""")
     results = cursor.fetchall()
     error_sum = results[0]
     date = results[0][0]
